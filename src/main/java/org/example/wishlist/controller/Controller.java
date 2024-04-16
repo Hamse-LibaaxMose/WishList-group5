03bfe5dcf95c721da088c1d0e1d9a01ec174ecaa
tgavri <thom54s8@stud.kea.dk>
2024-04-16 10:47:52 +0200

    init

--- src/main/java/org/example/wishlist/controller/Controller.java
+++ src/main/java/org/example/wishlist/controller/Controller.java
@@ -1,4 +1,11 @@
 package org.example.wishlist.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
 public class Controller {
+
+    @GetMapping("/")
+    public String showIndex(){
+    return "index";}
 }
